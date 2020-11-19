be36f670d5ca736decd7dbe49941750af0e690eb
Rahul Prajapati <rahul.prajapati@live.in>
2020-11-19 15:40:22 +0530

    Fix phpunit travis-ci issues. - Use same phpunit 7.5.20 version for all tests. - Remove php 7.1 test.

--- bin/run-wp-unit-tests.sh
+++ bin/run-wp-unit-tests.sh
@@ -9,17 +9,18 @@ composer install
 if [[ ! -z "$WP_VERSION" ]] ; then
   echo -en "travis_fold:start:install_wp_tests\r"
   bash ./bin/install-wp-tests.sh wordpress_test root '' localhost $WP_VERSION
-  composer global require "phpunit/phpunit=7.5.*"
   echo -en "travis_fold:end:install_wp_tests\r"
 fi
 
 echo "Running with the following versions:"
+echo "php -v"
 php -v
-phpunit --version
+echo "./vendor/bin/phpunit --version"
+./vendor/bin/phpunit --version
 
 # Run PHPUnit tests
 if [[ ! -z "$WP_VERSION" ]] ; then
-  phpunit --coverage-clover=clover.xml || exit 1;
+  ./vendor/bin/phpunit --coverage-clover=clover.xml || exit 1;
 fi
 
 # Run phpcs
